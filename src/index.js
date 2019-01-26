2b084fe2609661f0666546a4365cb498dc1a9706
Wioletta Gluza <wiolagluza@gmail.com>
2019-01-26 17:19:21 +0100

    initial colour palette

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {I18nextProvider} from 'react-i18next';
+import i18next from 'i18next';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import common_en from "./translations/en/common.json";
+import common_pl from "./translations/pl/common.json";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+i18next.init({
+    interpolation: { escapeValue: false }, // React already does escaping
+    lng: "en", // default language
+    resources: {
+      en: {
+        common: common_en // 'common' is our custom namespace
+      },
+      pl: {
+        common: common_pl
+      }
+    }
+  });
+
+  ReactDOM.render(
+    <I18nextProvider i18n={i18next}>
+      <App/>
+    </I18nextProvider>,
+    document.getElementById("root")
+  );
+
 serviceWorker.unregister();
